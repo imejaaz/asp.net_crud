702cb1175d2177e2499c7a2cdb7a3bac244be66d
Ijaz Ahmad <Ijaz.professional@gmail.com>
2024-06-23 08:48:28 +0500

    project done

---
+++ StudentLMS/StudentLMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentLMS.Models.Entities;
+
+namespace StudentLMS.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
